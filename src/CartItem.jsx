16b042d7426e5dd1dfd732393d2410d754a949bb
yukl-c <yulkchen@gmail.com>
2025-06-06 22:14:37 -0400

    task 1

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch,  } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,7 +7,19 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const newItem = action.payload;
+        const existingItem = state.items.find(item => item.name === newItem.name);
+        
+        if (existingItem) {
+            // If item already exists, increase quantity
+            existingItem.quantity += 1;
+        } else {
+            // Add new item with quantity 1
+            state.items.push({
+            ...newItem,
+            quantity: 1
+            });
+        }
     },
     removeItem: (state, action) => {
     },
